c8d3214b493298823c250ec8d4a3c10a5829520e
Erastus Murungi <erastuswagwan@gmail.com>
2024-06-03 18:18:01 -0700

    add shadcdn properly

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center">Hello, world!</h1>
+      <Input placeholder="Type something" />
+    </div>
+  );
+}
