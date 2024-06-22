542ca1c886098634c6a4f840a36c12e624b6ffab
Erastus Murungi <erastuswagwan@gmail.com>
2024-06-21 18:07:11 -0700

    add some bais shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import { Input } from "@/components/ui/input";
+"use client";
+
+import styled from "@emotion/styled";
+import { CardsCreateAccount } from "@/app/components/create-account";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+
+const CentrallyPlacedDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl text-center">Hello, world!</h1>
-      <Input placeholder="Type something" />
+      <CentrallyPlacedDiv>
+        <CardsCreateAccount />
+      </CentrallyPlacedDiv>
     </div>
   );
 }
